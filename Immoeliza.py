1b49392616efdb2d4c9ee663a77b361203ee789e
gietsc <58869416+gietsc@users.noreply.github.com>
2020-09-25 08:56:07 +0200

    Update Immoeliza.py

--- Immoeliza.py
+++ Immoeliza.py
@@ -15,44 +15,44 @@ headers = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
 
 n_pages=0
-for page in range(0,9):
+for page in range(0,11):
     n_pages+=1
-    url= 'https://www.realo.be/en/search/for-sale' +'?page='+str(page)
+    url= 'https://www.realo.be/en/search/for-sale?page='+str(page)
     req= get(url, headers=headers)
     html_soup = BeautifulSoup(req.text, 'html.parser')
 
 
-price=[]
-typeofproperty=[]
-locality=[]
+    price=[]
+    typeofproperty=[]
+    locality=[]
 
-house_containers = html_soup.find_all('li')
+    house_containers = html_soup.find_all('li')
 
-for tag in html_soup.find_all('div',attrs={"data-type":"label"}):
-    row = tag.get_text()
-    if not row:
-        row = "NA"
+    for tag in html_soup.find_all('div',attrs={"data-type":"label"}):
+        row = tag.get_text()
+        if not row:
+            row = "NA"
 
-    price.append(row)
-print(price)
+        price.append(row)
+    print(price)
 
-for elem in html_soup.find_all('span',attrs={"class":"type truncate"}):
-    type = list(elem)
-    types = [n.strip() for n in type]
-    if not types:
-        types="NA"
-    typeofproperty.append(types)
-print(typeofproperty)
+    for elem in html_soup.find_all('span',attrs={"class":"type truncate"}):
+        type = list(elem)
+        types = [n.strip() for n in type]
+        if not types:
+            types="NA"
+        typeofproperty.append(types)
+    print(typeofproperty)
 
 
-for n in html_soup.find_all('div',attrs={"class":"address truncate"}):
-    local= [n.text]
-    locals = [i.strip() for i in local]
-    if not locals:
-        locals="NA"
-    locality.append(locals)
-print(locality)
+    for n in html_soup.find_all('div',attrs={"class":"address truncate"}):
+        local= [n.text]
+        locals = [i.strip() for i in local]
+        if not locals:
+            locals="NA"
+        locality.append(locals)
+    print(locality)
 
-print(len(locality))
-print(len(price))
-print(len(typeofproperty))
+    print(len(locality))
+    print(len(price))
+    print(len(typeofproperty))
