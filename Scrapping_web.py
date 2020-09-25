7c50075b1a588499bd3a046652159ea297eb067f
Saba Yahyaa <sabayahyaa@gmail.com>
2020-09-25 13:58:55 +0200

    adding extra features from houses, land and flats

--- Scrapping_web.py
+++ Scrapping_web.py
@@ -23,24 +23,8 @@ with open('myurllist-2.csv', 'r') as f:
     #urls = list(csv.reader(f))
 
 
-# print(urls[0])
-# print(urls[1])
-# pdb.set_trace()
 
 
-############"
-#####  TO DO....................
-#############
-#ask christope for correct links, I will scap
-urls=urls[0:5]
-
-
-
-
-
-# "https://www.trulia.com/for_rent/Oakland,CA/1p_beds/SINGLE-FAMILY_HOME_type
-# url="https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE"
-#url=urls[0][0]  #get the url from Christope list
 
 #Note: when we have a list of urls, Beautifulsoup gives an error to overcome this problem,
 # we have either use get_headers or urllib.request (this is not working), so I need to try with urllib3 later
@@ -61,10 +45,11 @@ ctx.verify_mode = ssl.CERT_NONE
 
 locality=[] #ok
 typeofproperty=[] #ok
-price=[]
-numberofrooms=[]  #ok
+price=[] #ok
+NumberBedRooms=[]  #ok, bedrooms
+NumberBathrooms=[] #ok,
 kitchenfullyequipped=[]
-area=[]
+HabitableArea=[]
 furnished=[]
 openfire=[]
 Surface_area_of_the_plot_of_land=[]
@@ -80,8 +65,8 @@ Surface_area_of_PlotLand=[]
 Number_of_facades=[]
 Swimming_pool=[]#(Yes / No)
 State_of_Building=[]  #(New, to be renovated, ...)
-
-
+HeatingType=[] #ok
+ElectricalInspectionReport=[] #ok Electrical inspection report
 
 # for url in urls: #extract from te csv file
 # count=1 # for pagination
@@ -96,7 +81,22 @@ print("------------------------------")
 print("------------------------------")
 print("------------------------------")
 print("------------------------------")
-# urls=[urls[0][0], urls[1][0]]
+
+# print(urls[0])
+# print(urls[1])
+# pdb.set_trace()
+
+
+############"
+#####  TO DO....................
+#############
+#ask christope for correct links, I will scap
+urls=urls[0:15]
+#urls=urls[8:10]
+
+# "https://www.trulia.com/for_rent/Oakland,CA/1p_beds/SINGLE-FAMILY_HOME_type
+# url="https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE"
+
 
 for url in urls:
     print(url)
@@ -110,17 +110,28 @@ for url in urls:
     print(url, r.status_code)
     soup = BeautifulSoup(r.content,'html.parser')
 
+    #extract locality
     container = soup.find_all('div', attrs={"class":"property__container"})
     for c in container:  #price, locality, from Dilara
             for link in c.findAll('h1', attrs={'class':"address"}):
              # print(link.text)
               locality.append(link.text.strip())
-
+    # extract price
     for c in container:  #price, locality, type of property from Dilara
             for link in c.findAll("span", attrs={"itemprop":"price"}):
              # print(link.text)
               price.append(link.text)
 
+   # extract type_of_property
+    for c in container:  # price, locality, type of property from Dilara
+        for link in c.findAll("div", attrs={"class": "type"}):
+            typeofproperty.append(link.text.partition(' ')[0].strip()) #get the first word only
+            Type_checking=link.text.partition(' ')[0].strip() #the table of html file is different depending on
+                                           # the type of property (flat or house), so check if you have flat or house
+
+    print(Type_checking)
+
+    # 444444444444444444444444444444444444444
     table = soup.find_all("table")
     #print(table[1])
     #to iterate for all tables
@@ -128,33 +139,110 @@ for url in urls:
     #     print(each_table.find("tbody"))
     #     print("----------------------------")
 
-    #I want only table[1] to scrap it
 
-    table = soup.find_all("table")
-    rows=table[1]
-    a=[]
 
-    # for item in rows.find_all("tr"):
-    #     for sub_item in item.find_all("td"):
-    #          a.append(sub_item.text.strip().replace(',',''))
+    if (Type_checking == "House" or Type_checking == "Land"):
+        # I want only table[1] to scrap features of houses
+        rows=table[1]
+        # a=[]
+        # for item in rows.find_all("tr"):
+        #     for sub_item in item.find_all("td"):
+        #          a.append(sub_item.text.strip().replace(',',''))
+        # print(table)
 
-    results = {}
-    for item in rows.find_all("tr"):
-        aux = item.findAll('td')
-        results[aux[0].string] = aux[1].string
+        # pdb.set_trace()
 
-    #print (results)
-    # for k, v in results.items():
-    #     print(k, v)
-    #appending to lists:
-    if results['Property type'] !=None:
-        typeofproperty.append(results['Property type'].strip())
+        # 5555555555555555555555555
+        # 5555555555555555555555555
+        # 5555555555555555555555555
 
-    if 'Bedrooms' in results: #we have land for sale, so there is no bedroom key
-        numberofrooms.append(results['Bedrooms'])
+        # <table>
+        #     < tbody >
+        #     < tr >
+        #         < td >
+        #             < td > #I could not scrap here?????????????????????????????, the value of area for example
+        # 5555555555555555555555555
+        # 5555555555555555555555555
+        # 5555555555555555555555555
+
+
+        results = {}
+        for item in rows.find_all("tr"):
+            aux = item.findAll('td')
+            results[aux[0].string] = aux[1].string
+
+        # print (results)
+        #
+        # pdb.set_trace()
+        # for k, v in results.items():
+        #     print(k, v)
+
+        #bed rooms
+        if 'Bedrooms' in results: #we have land for sale, so there is no bedroom key
+            NumberBedRooms.append(results['Bedrooms'])
+        else:
+            NumberBedRooms.append(0)
+
+        #Habitable area
+        if 'Bathrooms' in results: #we have land for sale, so there is no bedroom key
+            NumberBathrooms.append(results['Bathrooms'])
+        else:
+            NumberBathrooms.append(0)
+
+        #Heating type
+        if 'Heating type' in results: #we have land for sale, so there is no bedroom key
+            HeatingType.append(results['Heating type'].strip())
+        else:
+            HeatingType.append('None')
+
+        #Electrical inspection report
+        if 'Electrical inspection report' in results:  # we have land for sale, so there is no bedroom key
+            ElectricalInspectionReport.append(results['Electrical inspection report'].strip())
+        else:
+            ElectricalInspectionReport.append('None')
+
+    # end here 7777777777777777777777777777777777777777777
+# end here 7777777777777777777777777777777777777777777
+
+    elif (Type_checking == "Flat"):
+        # 55555555555555555555555555555555
+        # 55555555555555555555555555555555
+        # I want only table[0] to scrap features of flats
+        rows=table[0]
+        results = {}
+        for item in rows.find_all("tr"):
+            aux = item.findAll('td')
+            results[aux[0].string] = aux[1].string
+
+        if 'Bedrooms' in results: #we have land for sale, so there is no bedroom key
+            NumberBedRooms.append(results['Bedrooms'])
+        else:
+            NumberBedRooms.append(0)
+
+            # area
+            # Habitable area
+        if 'Bathrooms' in results:  # we have land for sale, so there is no bedroom key
+                NumberBathrooms.append(results['Bathrooms'])
+        else:
+                NumberBathrooms.append(0)
+
+        # Heating type
+        if 'Heating type' in results:  # we have land for sale, so there is no bedroom key
+            HeatingType.append(results['Heating type'].strip())
+        else:
+            HeatingType.append(0)
+
+        # Electrical inspection report
+        if 'Electrical inspection report' in results:  # we have land for sale, so there is no bedroom key
+            ElectricalInspectionReport.append(results['Electrical inspection report'].strip())
+        else:
+            ElectricalInspectionReport.append('None')
+
+        # 55555555555555555555555555555555
+        # 55555555555555555555555555555555
     else:
-        numberofrooms.append(0)
-
+        pass
+    # 444444444444444444444444444444444444444
 #{ 'Bathrooms': '1',
 # 'Habitable area': None,
 # 'Facade width': '9,3m',
@@ -177,5 +265,20 @@ print("------------------------------------")
 print(locality)
 print(typeofproperty)
 print(price)
-print(numberofrooms)
+print(NumberBedRooms)
+print(NumberBathrooms)
+print(HeatingType)
+print(ElectricalInspectionReport)
 
+#create a data frame
+dic={'locality':locality,
+     'typeofproperty':typeofproperty,
+     'price':price,
+     'NumberBedRooms':NumberBedRooms,
+     'NumberBathrooms':NumberBathrooms,
+     'HeatingType':HeatingType,
+     'ElectricalInspectionReport':ElectricalInspectionReport }
+
+df=pd.DataFrame(dic)
+#print(df.head())
+df.to_csv('RealStateData.csv', encoding='utf-8', index=False)
