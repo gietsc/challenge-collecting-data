818567c618b805d6c6da6faf64896577bf768840
gietsc <58869416+gietsc@users.noreply.github.com>
2020-09-25 08:49:50 +0200

    Update Immoeliza.py

--- Immoeliza.py
+++ Immoeliza.py
@@ -1,24 +1,3 @@
-#CHRIS'S SIDE
-from bs4 import BeautifulSoup
-import requests
-
-url='https://www.realo.be/en/search/for-sale'
-r = requests.get(url)
-print(url, r.status_code)
-soup = BeautifulSoup(r.content,'lxml')
-
-links=[]
-for elem in soup.find_all('a',attrs={"class" :"link"}):
-    links.append(elem.get('href'))
-
-links_properties=['https://www.realo.be'+ elem for elem in links]
-
-print(links_properties)
-
-# PLEASE MENTION IN THE GROUP ONCE YOU CHANGE THIS FILE.
-
-#DEMO FROM TOWARDS DATA SCIENCE, USE THIS TO TRY THE ATTRIBUTES
-
 from bs4 import BeautifulSoup
 import re
 from urllib.request import urlopen, Request
@@ -26,6 +5,8 @@ from requests import get
 import requests
 import pandas as pd
 import itertools
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
@@ -33,21 +14,45 @@ sns.set()
 headers = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
 
-url='https://www.realo.be/en/search/for-sale'
-response = get(url, headers=headers)
-print(response)
-r= requests.get(url)
-print(url,r.status_code)
-html_soup = BeautifulSoup(response.text, 'html.parser')
+n_pages=0
+for page in range(0,9):
+    n_pages+=1
+    url= 'https://www.realo.be/en/search/for-sale' +'?page='+str(page)
+    req= get(url, headers=headers)
+    html_soup = BeautifulSoup(req.text, 'html.parser')
 
-print('TADAA')
+
+price=[]
+typeofproperty=[]
+locality=[]
+
 house_containers = html_soup.find_all('li')
-print(house_containers)
-print('!!HERE WE ARE!!!')
 
 for tag in html_soup.find_all('div',attrs={"data-type":"label"}):
-    prices = list(tag)
-    print(prices)
-print('!!PRICE TAGS NOT HASHTAGS!!')
+    row = tag.get_text()
+    if not row:
+        row = "NA"
+
+    price.append(row)
+print(price)
+
+for elem in html_soup.find_all('span',attrs={"class":"type truncate"}):
+    type = list(elem)
+    types = [n.strip() for n in type]
+    if not types:
+        types="NA"
+    typeofproperty.append(types)
+print(typeofproperty)
 
 
+for n in html_soup.find_all('div',attrs={"class":"address truncate"}):
+    local= [n.text]
+    locals = [i.strip() for i in local]
+    if not locals:
+        locals="NA"
+    locality.append(locals)
+print(locality)
+
+print(len(locality))
+print(len(price))
+print(len(typeofproperty))
